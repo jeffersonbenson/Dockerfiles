8f7bbf44453084a29c8820441010779505a7ba4a
jeffersonbenson <wolfman18336@gmail.com>
2017-09-11 17:03:57 -0400

    progress and documentation

--- Wordpress/Wordpress/wp-config.php
+++ Wordpress/Wordpress/wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', 'example');
 
 /** MySQL hostname */
-define('DB_HOST', 'mysql');
+define('DB_HOST', 'db');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
